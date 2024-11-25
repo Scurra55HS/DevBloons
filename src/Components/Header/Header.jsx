de9591372f43d772e097522d7555da691548961e
Anthony Bruno <138811075+Scurra55HS@users.noreply.github.com>
2024-11-25 14:02:58 -0300

    Add files via upload
    ja pode?

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,17 @@
+// import NavBar from '../Nav/Nav';
+// import LogoImage from '../../img/ClearTrackLogo.png';
+import { DivTi, HeaderContainer, Title } from '../../Styles/Header';
+
+const Header = () => {
+    return (
+        <HeaderContainer>
+            {/* <Logo src={LogoImage} alt="Logo" /> */}
+            <DivTi>
+            <Title>DEVBLOONS</Title>
+            </DivTi>
+            {/* <NavBar /> */}
+        </HeaderContainer>
+    );
+};
+
+export default Header;
